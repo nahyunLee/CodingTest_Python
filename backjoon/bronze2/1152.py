cb654fc3aa794b471d16f6e598fad64ca41068e3
nahyunLee <sksms304@gmail.com>
2022-05-05 23:33:15 +0900

    1152

--- backjoon/bronze2/1152.py
+++ backjoon/bronze2/1152.py
@@ -1,2 +1,18 @@
-result = list(map(str, input().split()))
-print(len(result))
+size = int(input())
+pc = []
+people = list(map(int, input().split()))
+refuse = 0
+flag = False
+for i in range(size):
+    want = people[i]
+    for z in range(len(pc)):
+        if want == pc[z]:
+            refuse +=1
+            flag = True
+            break
+    if flag is False:
+        pc.append(want)
+    flag = False
+
+print(refuse)
+
